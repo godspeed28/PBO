975d59623278768b11dedbfff5989530354acd49
goodsped <albertog4taz28@gmail.com>
2025-05-12 10:45:14 +0700

    logic exit

--- week_8/BangunDatar.java
+++ week_8/BangunDatar.java
@@ -1,6 +1,6 @@
 // package week_8;
 
-class BangunDatar{
+class BangunDatar {
     protected int sisi;
     protected int lebar;
     protected int panjang;
@@ -12,34 +12,51 @@ class BangunDatar{
     protected String stat = "square";
     protected final double PHI = 3.14;
 
-    public BangunDatar(int sisi){
+    public BangunDatar(int sisi) {
         this.sisi = sisi;
     }
-    public BangunDatar(int panjang,int lebar){
+
+    public BangunDatar(int panjang, int lebar) {
         this.panjang = panjang;
         this.lebar = lebar;
         this.stat = "rectangle";
     }
-    public BangunDatar(double alas,double tinggi){
+
+    public BangunDatar(double alas, double tinggi, String stat) {
         this.alas = alas;
         this.tinggi = tinggi;
-        this.stat = "triangle";
+        this.stat = stat;
 
     }
-    public BangunDatar(double radius){
+
+    public BangunDatar(double radius, String stat) {
         this.radius = radius;
-        this.stat = "circle";
+        this.stat = stat;
 
     }
-    public void luas(BangunDatar bd){
-        if(bd.stat.equals("square")){
-            this.luas = bd.sisi*bd.sisi;
+
+    public void luas(BangunDatar bd) {
+        if (bd.stat.equals("square")) {
+            this.luas = bd.sisi * bd.sisi;
+        }
+        if (bd.stat.equals("rectangle")) {
+            this.luas = bd.panjang * bd.lebar;
+        }
+        if (bd.stat.equals("triangle")) {
+            this.luas2 = 0.5 * bd.alas * bd.tinggi;
+        }
+        if (bd.stat.equals("circle")) {
+            this.luas2 = PHI * bd.radius * bd.radius;
         }
 
     }
 
-    public void getLuas(){
+    public void getLuas() {
         System.out.println(this.luas);
-        
+
+    }
+
+    public void getLuas2() {
+        System.out.println(this.luas2);
     }
 }
--- week_8/Main.java
+++ week_8/Main.java
@@ -1,10 +1,134 @@
 // import BangunDatar;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args ){
-        BangunDatar persegi = new BangunDatar(5);
-        persegi.luas(persegi);
-        persegi.getLuas();
+    public static void display() {
+        System.err.println("Pilih aksi:");
+        System.err.print(
+                "1. Persegi\n" + "2. Persegi Panjang\n" + "3. Segitiga\n" + "4. Lingkaran\n" + "5. Keluar\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int pil;
+        String pil2 = "y";
+        String stat = "selesai";
+        int angka;
+        int angka2;
+
+        double dangka;
+        double dangka2;
+
+        System.out.println("Menghitung Luas Bangun Datar");
+
+        do {
+            display();
+            System.out.print("> ");
+            pil = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (pil) {
+
+                case 1:
+                    System.out.println("Menghitung Luas Persegi");
+                    System.out.print("Masukan Sisi = ");
+
+                    angka = scanner.nextInt();
+                    scanner.nextLine();
+
+                    BangunDatar persegi = new BangunDatar(angka);
+                    persegi.luas(persegi);
+
+                    System.out.print("Luas Persegi = ");
+                    persegi.getLuas();
+
+                    stat = "hitung";
+
+                    break;
+
+                case 2:
+                    System.out.println("Menghitung Luas Persegi Panjang");
+                    System.out.print("Masukan Panjang = ");
+
+                    angka = scanner.nextInt();
+                    scanner.nextLine();
+
+                    System.out.print("Masukan Lebar = ");
+
+                    angka2 = scanner.nextInt();
+                    scanner.nextLine();
+
+                    BangunDatar persegiPanjang = new BangunDatar(angka, angka2);
+                    persegiPanjang.luas(persegiPanjang);
+
+                    System.out.print("Luas Persegi Panjang = ");
+                    persegiPanjang.getLuas();
+
+                    stat = "hitung";
+
+                    break;
+
+                case 3:
+                    System.out.println("Menghitung Luas Segitiga");
+                    System.out.print("Masukan Alas = ");
+
+                    dangka = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    System.out.print("Masukan Tinggi = ");
+
+                    dangka2 = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    BangunDatar segitiga = new BangunDatar(dangka, dangka2, "triangle");
+                    segitiga.luas(segitiga);
+
+                    System.out.print("Luas Segitiga = ");
+                    segitiga.getLuas2();
+
+                    stat = "hitung";
+
+                    break;
+
+                case 4:
+                    System.out.println("Menghitung Luas Lingkaran");
+                    System.out.print("Masukan Jari-Jari = ");
+
+                    dangka = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    BangunDatar lingkaran = new BangunDatar(dangka, "circle");
+                    lingkaran.luas(lingkaran);
+
+                    System.out.print("Luas lingkaran = ");
+                    lingkaran.getLuas2();
+
+                    stat = "hitung";
+
+                    break;
+
+                case 5: // Keluar
+                    System.out.println("Keluar...");
+                    stat = "selesai";
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid!");
+                    break;
+            }
+
+            if (!stat.equals("selesai")) {
+                System.out.print("Hitung lagi? (y/n): ");
+                pil2 = scanner.nextLine();
+            }
+
+        } while (pil != 5 && !pil2.equalsIgnoreCase("n"));
+
+        if (pil2.equals("n")) {
+            System.out.println("Keluar...");
+        }
 
     }
 }
